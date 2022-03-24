bc03e0536117ffa9698014dbd636320b5ad087c9
Sergey <sergsmile29@gmail.com>
2022-03-25 00:28:31 +0200

    my box shadow generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ControlBox from './ControlBox.js'
+
+function App(){
+    const [shadows, setShadows] = useState([])
+    const [controls, setControls] = useState([
+        {
+            index: 0
+        }
+    ])
+
+    const updateShadow = (s, id) => {
+        const t = [...shadows]
+        t[id] = s.inset ? `inset ${s.x}px ${s.y}px ${s.blur}px ${s.spread}px ${s.color}` : `${s.x}px ${s.y}px ${s.blur}px ${s.spread}px ${s.color}`
+        setShadows(t)
+    }
+
+    function addShadow(){
+        setControls([...controls, {
+            index: controls.length
+        }])
+    }
+
+    return (
+        <div className="container">
+            <div className="preview">
+                <div className="preview-box" style={{boxShadow: shadows.join(",")}}></div>
+            </div>
+            <div className="controls">
+                {controls.map(c => <ControlBox key={c.index} id={c.index} updateShadow={updateShadow}/>)}
+                <p className="text-right">
+                    <button onClick={addShadow}>Add Layer</button>
+                </p>
+                <div className="codes controls-box">
+                    <span className="prop-name">box-shadow: </span>
+                    <span className="code">{shadows.join(", ")};</span>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/ControlBox.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+
+
+function ControlBox({updateShadow, id}){
+
+    const [shadowModel, setShadowModel] = useState({
+        x: 0,
+        y: 0,
+        blur: 0,
+        spread: 0,
+        color: '#000',
+        inset: false
+    })
+
+    const updateShadowModel = (propr, val) => {
+        setShadowModel({
+            ...shadowModel,
+            [propr]: val
+        })
+    }
+
+    useEffect(() => {
+        updateShadow(shadowModel, id)
+    }, [shadowModel])
+
+    return (
+        <div className="controls-box">
+            <label>Offset X</label>
+            <input type="range" min="-100" max="100" defaultValue="0" onChange={(e) => updateShadowModel("x", e.target.value)} />
+            <label>Offset Y</label>
+            <input type="range" min="-100" max="100" defaultValue="0" onChange={(e) => updateShadowModel("y", e.target.value)} />
+            <label>Blur</label>
+            <input type="range" min="0" max="100" defaultValue="0" onChange={(e) => updateShadowModel("blur", e.target.value)} />
+            <label>Spread</label>
+            <input type="range" min="0" max="100" defaultValue="0" onChange={(e) => updateShadowModel("spread", e.target.value)} />
+            <label>Color</label>
+            <input type="color" onChange={(e) => updateShadowModel("color", e.target.value)} />
+            <p>
+                <span style={{marginRight: ".5em"}}>Inset:</span>
+                <input type="checkbox" onChange={(e) => updateShadowModel("inset", !shadowModel.inset)} />
+            </p>
+        </div>
+    )
+}
+
+export default ControlBox
